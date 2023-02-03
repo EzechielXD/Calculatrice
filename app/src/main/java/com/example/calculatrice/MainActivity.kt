7d58d66e8eabeeab2524bb938833f5b601c256d2
EzechielXD <ezechielkouame57@gmail.com>
2023-02-03 21:45:42 +0000

    dernier commit

--- app/src/main/java/com/example/calculatrice/MainActivity.kt
+++ app/src/main/java/com/example/calculatrice/MainActivity.kt
@@ -12,21 +12,60 @@ import androidx.core.text.isDigitsOnly
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    var isNewOperation = true
-    var oldNumber = ""
-    var op = "+"
-    var moins = false
-    var virgule = false
+    var DernierNombre = ""
+    var PremierNombre = ""
+    var Operator = ""
+    var isOperatorClick = false
+    var isEqualClick = false
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.setTitle("Ma super Calco")
         editText.setTextIsSelectable(true)
         editText.isFocusable = false
         editText.isClickable = false
         editText.isLongClickable = false
+
+
+        bu0.setOnClickListener{numberEvent("0", true)}
+        bu1.setOnClickListener{numberEvent("1", true)}
+
+        bu2.setOnClickListener { numberEvent("2", true) }
+
+        bu3.setOnClickListener { numberEvent("3", true) }
+
+        bu4.setOnClickListener { numberEvent("4", true) }
+
+        bu5.setOnClickListener { numberEvent("5", true) }
+
+        bu6.setOnClickListener { numberEvent("6", true) }
+
+        bu7.setOnClickListener { numberEvent("7", true) }
+
+        bu8.setOnClickListener { numberEvent("8", true) }
+
+        bu9.setOnClickListener { numberEvent("9", true) }
+
+        buPlus.setOnClickListener { operatorEvent("+") }
+
+        buMoins.setOnClickListener { operatorEvent("-") }
+
+        buMultiplication.setOnClickListener { operatorEvent("*") }
+
+        buDivision.setOnClickListener { operatorEvent("÷") }
+
+        buModulo.setOnClickListener { operatorEvent("%") }
+
+        buPlusMoins.setOnClickListener { inverse() }
+
+        buegal.setOnClickListener { equalEvent() }
+
+            buDel.setOnClickListener { del() }
+
+                buAC.setOnClickListener { resetEvent() }
 
 
         editText.setOnClickListener{
@@ -44,6 +83,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         var text: String
@@ -58,116 +98,162 @@ class MainActivity : AppCompatActivity() {
         editText.setText(newText)
     }
 
-    fun numberEvent(view: View){
-        if(isNewOperation)
-            editText.setText("")
 
-        isNewOperation = false
-        var buttonClick = editText.text.toString()
-        var buttonSelect = view as Button
-        var negatif = 0
-        when(buttonSelect.id){
-            bu0.id -> {buttonClick += "0"}
-            bu1.id -> {buttonClick += "1"}
-            bu2.id -> {buttonClick += "2"}
-            bu3.id -> {buttonClick += "3"}
-            bu4.id -> {buttonClick += "4"}
-            bu5.id -> {buttonClick += "5"}
-            bu6.id -> {buttonClick += "6"}
-            bu7.id -> {buttonClick += "7"}
-            bu8.id -> {buttonClick += "8"}
-            bu9.id -> {buttonClick += "9"}
-            buPlusMoins.id -> {
-                if(moins == false)
-            {
-                buttonClick = "-"+buttonClick
-            }
-                moins = false
-
-            }
-
+    private fun numberEvent(string: String, canClear: Boolean) {
+        if (isOperatorClick || isEqualClick || editText.text.isNotEmpty() && canClear) {
+            isOperatorClick = false
+            isEqualClick = false
         }
-        editText.setText(buttonClick)
+        editText.append(string)
+        DernierNombre += string
     }
 
-    fun operatorEvent(view: View){
-        isNewOperation = true
-        oldNumber = editText.text.toString()
-        var buttonSelect = view as Button
-        when(buttonSelect.id){
-            buMultiplication.id -> {op = "×"}
-            buDivision.id -> {op = "÷"}
-            buPlus.id -> {op = "+"}
-            buMoins.id -> {op = "-"}
-            buModulo.id -> {op = "%"}
+    private fun operatorEvent(operateur: String) {
+        if (Operator!=""){
+
+            equalEvent()
         }
+        if (DernierNombre.isNotEmpty()) {
+            PremierNombre = DernierNombre
+            DernierNombre = ""
+            editText.append(operateur)
+            Operator = operateur
+            isOperatorClick = true
+        }
+    }
+
+    private fun equalEvent() {
+        if (DernierNombre.isNotEmpty() && PremierNombre.isNotEmpty() && Operator.isNotEmpty()) {
+            val resultCalcul =calc()
+            editText.text = resultCalcul.toString()
+            isEqualClick = true
+            DernierNombre = resultCalcul.toString()
+            PremierNombre = ""
+            Operator = ""
+        }
+
+
 
     }
 
-    fun equalEvent(view: View){
+    private fun calc(): Any {
 
-        var newNumber = editText.text.toString()
-        var result = 0.0;
-        var result_int : Int;
-
-
-        when(op){
-            "×" -> {
-                if((oldNumber.isNotEmpty() && oldNumber.isDigitsOnly()) && (newNumber.isNotEmpty() && newNumber.isDigitsOnly())){
-                    result_int = (oldNumber.toDouble() * newNumber.toDouble()).toInt()
-                    editText.setText(result_int.toString())
-                }else{
-                    result = oldNumber.toDouble() * newNumber.toDouble()
-                    editText.setText(result.toString())
-                }}
+        return when (Operator) {
             "+" -> {
-                if((oldNumber.isNotEmpty() && oldNumber.isDigitsOnly()) && (newNumber.isNotEmpty() && newNumber.isDigitsOnly())){
-                    result_int = (oldNumber.toDouble() + newNumber.toDouble()).toInt()
-                    editText.setText(result_int.toString())
-                }else{
-                    result = oldNumber.toDouble() + newNumber.toDouble()
-                    editText.setText(result.toString())
+                try {
+                    PremierNombre.toInt() + DernierNombre.toInt()
+                }
+                catch (e:java.lang.NumberFormatException){
+                    PremierNombre.toDouble() + DernierNombre.toDouble()
                 }
             }
-            "÷" -> {result = oldNumber.toDouble() / newNumber.toDouble()
-                editText.setText(result.toString())}
             "-" -> {
-                if((oldNumber.isNotEmpty() && oldNumber.isDigitsOnly()) && (newNumber.isNotEmpty() && newNumber.isDigitsOnly())){
-                    result_int = (oldNumber.toDouble() - newNumber.toDouble()).toInt()
-                    editText.setText(result_int.toString())
-                }else{
-                    result = oldNumber.toDouble() - newNumber.toDouble()
-                    editText.setText(result.toString())
+                try {
+                    PremierNombre.toInt() - DernierNombre.toInt()
+                } catch (e: java.lang.NumberFormatException) {
+                    PremierNombre.toDouble() - DernierNombre.toDouble()
                 }
             }
-            "%" -> {
-                if((oldNumber.isNotEmpty() && oldNumber.isDigitsOnly()) && (newNumber.isNotEmpty() && newNumber.isDigitsOnly())){
-                    result_int = (oldNumber.toDouble() % newNumber.toDouble()).toInt()
-                    editText.setText(result_int.toString())
-                }else{
-                    result = oldNumber.toDouble() % newNumber.toDouble()
-                    editText.setText(result.toString())
+            "*" -> {
+                try {
+                    PremierNombre.toInt() * DernierNombre.toInt()
+                } catch (e: java.lang.NumberFormatException) {
+                    PremierNombre.toDouble() * DernierNombre.toDouble()
+                }
+            }
+            "÷" -> {
+
+                val prev = PremierNombre.toDouble()
+                val curr = DernierNombre.toDouble()
+                val resultat = prev / curr
+                if (resultat == resultat.toInt().toDouble()) {
+                    resultat.toInt()
+                } else {
+                    resultat
+                }
+            }
+            "%" ->{
+                try {
+                    PremierNombre.toInt() % DernierNombre.toInt()
+                } catch (e: java.lang.NumberFormatException) {
+                    PremierNombre.toDouble() % DernierNombre.toDouble()
+                }
+            }
+            else -> 0
+        }
+    }
+
+    private fun del() {
+        val text = editText.text.toString()
+        if (text.isNotEmpty()) {
+            editText.text = text.substring(0, text.length - 1)
+            if (isOperatorClick) {
+                Operator = ""
+                isOperatorClick = false
+
+            } else {
+                val lastChar = text[text.length - 1]
+                if(text[0] == '-' && text.length == 2 ){
+                    resetEvent() // On retire le signe "-" et le chiffre
+                    return
+                }
+
+                else if(text.length > 2 && text[text.length - 2] == '-' && text[text.length - 1].isDigit()){
+                    editText.text = text.substring(0, text.length - 2)
+                    DernierNombre = DernierNombre.substring(0, DernierNombre.length - 1)
+                    isOperatorClick = false
+                    Operator = ""
+
+
+                }
+
+                else {
+                    if (DernierNombre.length == 0) {
+                        DernierNombre = "0"
+                        isOperatorClick = false
+                    }
+                    else  DernierNombre = DernierNombre.substring(0, DernierNombre.length - 1)
+
                 }
             }
         }
-
-
-
     }
 
-    fun resetEvent(view: View){
-        editText.setText("0")
-        isNewOperation = true
-    }
+    private fun inverse() {
+        if (DernierNombre.isNotEmpty()) {
+            if (DernierNombre.toIntOrNull() != null) DernierNombre =
+                DernierNombre.toInt().times(-1).toString()
+            else if (DernierNombre.toDoubleOrNull() != null) DernierNombre =
+                DernierNombre.toDouble().times(-1).toString()
+            else DernierNombre = DernierNombre.toInt().times(-1).toString()
+            if (PremierNombre.isNotEmpty()) {
+                editText.text = DernierNombre + Operator + DernierNombre
 
-    fun backspaceEvent(view: View){
-        if(editText.text.length > 0){
-            var newText = editText.text.substring(0, editText.length() - 1)
-            editText.setText(newText);
+            } else {
+                editText.text = DernierNombre
+            }
+        } else if (PremierNombre.isNotEmpty()) {
+            if (PremierNombre.toIntOrNull() != null) PremierNombre =
+                PremierNombre.toInt().times(-1).toString()
+            else if (PremierNombre.toDoubleOrNull() != null) PremierNombre =
+                PremierNombre.toDouble().times(-1).toString()
+            else PremierNombre = PremierNombre.toInt().times(-1).toString()
+
+
+            editText.text = PremierNombre
         }
     }
 
+    private fun resetEvent() {
+        editText.text = ""
+        DernierNombre = ""
+        PremierNombre = ""
+        Operator = ""
+        isOperatorClick = false
+        isEqualClick = false
+    }
 
 
-}
+
+    }
 
